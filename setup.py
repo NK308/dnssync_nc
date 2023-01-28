9bbdf7e1ccb8ee1e0c40ff2422ff09094731c548
Niclas Kretschmer <niclas.kretschmer@protonmail.com>
2023-01-28 17:12:11 +0100

    Adding packaging information

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+from setuptools import setup
+
+setup(
+    name="dnssync_nc",
+    version="0.1",
+    author="Johannes Bauer",
+    author_email="JohannesBauer@gmx.de",
+    license="GPL-3.0-only",
+    packages=['dnssync_nc']
+)
